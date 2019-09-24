221784f0247d4e8877b6293e331b654316ef3dba
darksign <dss2400@mail.ru>
2019-09-25 01:02:17 +0300

    homework 1 (5 screens)

--- app/src/main/java/com/example/myapplication/FifthActivity.java
+++ app/src/main/java/com/example/myapplication/FifthActivity.java
@@ -6,16 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class FifthActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_fifth);
     }
 
     public void onClick(View view) {
-            Intent intent = new Intent(this, SecondActivity.class);
+            Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
     }
 }
